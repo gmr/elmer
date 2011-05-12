6b15a0d213d2960900e25a0e11229d93626df86c
Gavin M. Roy <gmr@myyearbook.com>
2011-05-12 11:17:07 -0700

    Add virtual host to the toggle_monitoring function and restart rabbit_amqqueue, which gets rabbit to reload the new record but disconnects all consumers.  Publishers seem uneffected

--- rqae.erl
+++ rqae.erl
@@ -1,5 +1,5 @@
 -module(rqae).
--export([toggle_monitoring/1]).
+-export([toggle_monitoring/2]).
 -include("queue.hrl").
   
 get_new_bool_value(<<"True">>) ->
@@ -14,14 +14,17 @@ update_tuple_value({<<"x-monitor">>, DataType, Value}) ->
 update_tuple_value({Name, DataType, Value}) ->
   {Name, DataType, Value}.
 
-toggle_monitoring(Queue) ->
+toggle_monitoring(VirtualHost, Queue) ->
   F = fun() ->
     io:format("Running query~n"),
-    [A] = mnesia:read(rabbit_durable_queue, {resource,<<"/">>,queue,Queue}, write),
+    [A] = mnesia:read(rabbit_durable_queue, {resource,VirtualHost,queue,Queue}, write),
     io:format("Queue: ~s~n", [element(4, A#amqqueue.queue)]),
     Attributes = [update_tuple_value(Attrib) || Attrib <- A#amqqueue.attributes],
     New = A#amqqueue{attributes = Attributes},
     mnesia:write(rabbit_durable_queue, New, write)
   end,
   {_, Response} = mnesia:transaction(F),
+  %% I don't like this as it breaks consumer connections, need something better to get the data reloaded
+  rabbit_amqqueue:stop(),
+  rabbit_amqqueue:start(),
   Response.
