84a9d94c408d9096d90eab1dc1920da0fa3d6760
Gavin M. Roy <gmr@myyearbook.com>
2011-05-13 14:37:56 -0400

    Change x- to elmer- to keep within my own namespace for argument variable names.

--- elmer.erl
+++ elmer.erl
@@ -11,13 +11,13 @@ get_monitoring_status(VirtualHost, Queue) ->
   {Response, Record} = rabbit_amqqueue:lookup({resource, VirtualHost, queue, Queue}),
   case Response of
     ok ->
-      Monitor = get_tuple_value(Record#amqqueue.attributes, <<"x-monitor">>),
+      Monitor = get_tuple_value(Record#amqqueue.attributes, <<"elmer-monitor">>),
       case Monitor of
         error -> {false, false, -1, -1};
         _ -> {true,
-              get_tuple_value(Record#amqqueue.attributes, <<"x-monitor">>),
-              get_tuple_value(Record#amqqueue.attributes, <<"x-warn">>),
-              get_tuple_value(Record#amqqueue.attributes, <<"x-alert">>)}
+              get_tuple_value(Record#amqqueue.attributes, <<"elmer-monitor">>),
+              get_tuple_value(Record#amqqueue.attributes, <<"elmer-warn">>),
+              get_tuple_value(Record#amqqueue.attributes, <<"elmer-alert">>)}
       end;
     error -> {error, "Queue not found."}
   end.
@@ -28,31 +28,31 @@ setup_monitoring(VirtualHost, Queue, WarnQty, AlertQty) ->
   {Response, Record} = rabbit_amqqueue:lookup({resource, VirtualHost, queue, Queue}),
   case Response of
     ok ->
-      case get_tuple_value(Record#amqqueue.attributes, <<"x-monitor">>) of
+      case get_tuple_value(Record#amqqueue.attributes, <<"elmer-monitor">>) of
         true -> {error, "Monitoring already setup."};
         false -> {error, "Monitoring already setup."};
         error ->
-          Attributes = [{<<"x-monitor">>, bit, true},
-                        {<<"x-warn">>, short, WarnQty},
-                        {<<"x-alert">>, short, AlertQty}] ++ Record#amqqueue.attributes,
+          Attributes = [{<<"elmer-monitor">>, bit, true},
+                        {<<"elmer-warn">>, short, WarnQty},
+                        {<<"elmer-alert">>, short, AlertQty}] ++ Record#amqqueue.attributes,
           New = Record#amqqueue{attributes = Attributes},
           {write_record(New), Attributes}
       end;
     error -> {error, "Queue not found."}
   end.
 
-%% Toggles the boolean value of the x-monitor record.
+%% Toggles the boolean value of the elmer-monitor record.
 %% Returns a tuple of result, Value/Error message
 toggle_monitoring(VirtualHost, Queue) ->
   {Response, Record} = rabbit_amqqueue:lookup({resource, VirtualHost, queue, Queue}),
   case Response of
     ok ->
-      case get_tuple_value(Record#amqqueue.attributes, <<"x-monitor">>) of
+      case get_tuple_value(Record#amqqueue.attributes, <<"elmer-monitor">>) of
         error -> {error, "Monitoring not setup."};
         _ ->
-          Attributes = [toggle_x_monitor_value(Attrib) || Attrib <- Record#amqqueue.attributes],
+          Attributes = [toggle_elmer_monitor_value(Attrib) || Attrib <- Record#amqqueue.attributes],
           New = Record#amqqueue{attributes = Attributes},
-          {write_record(New), get_tuple_value(Attributes, <<"x-monitor">>)}
+          {write_record(New), get_tuple_value(Attributes, <<"elmer-monitor">>)}
       end;
     error -> {error, "Queue not found."}
   end.
@@ -63,7 +63,7 @@ change_monitoring_thresholds(VirtualHost, Queue, WarnQty, AlertQty) ->
   {Response, Record} = rabbit_amqqueue:lookup({resource, VirtualHost, queue, Queue}),
   case Response of
     ok ->
-      case get_tuple_value(Record#amqqueue.attributes, <<"x-monitor">>) of
+      case get_tuple_value(Record#amqqueue.attributes, <<"elmer-monitor">>) of
         error -> {error, "Monitoring not setup."};
         _ ->
           Attributes = [update_warn_or_alert_value(Attrib, WarnQty, AlertQty) || Attrib <- Record#amqqueue.attributes],
@@ -79,7 +79,7 @@ remove_monitoring(VirtualHost, Queue) ->
   {Response, Record} = rabbit_amqqueue:lookup({resource, VirtualHost, queue, Queue}),
   case Response of
     ok ->
-      case get_tuple_value(Record#amqqueue.attributes, <<"x-monitor">>) of
+      case get_tuple_value(Record#amqqueue.attributes, <<"elmer-monitor">>) of
         error -> {error, "Monitoring not setup."};
         _ ->
           Attributes = lists:filter(fun filter_monitoring_tuples/1, Record#amqqueue.attributes),
@@ -114,11 +114,11 @@ get_tuple_value(Attributes, Field) ->
     end,
   Value.
 
-%% Toggles the x-monitor attribute tuple's value
-toggle_x_monitor_value({<<"x-monitor">>, DataType, Value}) ->
-  {<<"x-monitor">>, DataType, get_new_bool_value(Value)};
+%% Toggles the elmer-monitor attribute tuple's value
+toggle_elmer_monitor_value({<<"elmer-monitor">>, DataType, Value}) ->
+  {<<"elmer-monitor">>, DataType, get_new_bool_value(Value)};
 
-toggle_x_monitor_value({Name, DataType, Value}) ->
+toggle_elmer_monitor_value({Name, DataType, Value}) ->
   {Name, DataType, Value}.
 
 %% flip the boolean value
@@ -131,19 +131,19 @@ get_new_bool_value(_) ->
 %% Only return attributes that are not related to monitoring
 filter_monitoring_tuples(Record) ->
   case element(1, Record) of
-    <<"x-monitor">> -> false;
-    <<"x-warn">> -> false;
-    <<"x-alert">> -> false;
+    <<"elmer-monitor">> -> false;
+    <<"elmer-warn">> -> false;
+    <<"elmer-alert">> -> false;
     _ -> true
   end.
 
-% Takes a tuple and updates the value for x-warn and x-alert
+% Takes a tuple and updates the value for elmer-warn and elmer-alert
 update_warn_or_alert_value(Record, WarnQty, AlertQty) ->
   case element(1, Record) of
-    <<"x-warn">> ->
-      {<<"x-warn">>, element(2, Record), WarnQty};
-    <<"x-alert">> ->
-      {<<"x-alert">>, element(2, Record), AlertQty};
+    <<"elmer-warn">> ->
+      {<<"elmer-warn">>, element(2, Record), WarnQty};
+    <<"elmer-alert">> ->
+      {<<"elmer-alert">>, element(2, Record), AlertQty};
     _ ->
       Record
   end.
